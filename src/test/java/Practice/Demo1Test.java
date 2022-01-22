7878c5bd28ace917dca89fac48c675871d0ceabd
91770 <91770@DESKTOP-IS2JCJD>
2022-01-22 20:09:43 +0530

    vinayak practice

--- src/test/java/Practice/Demo1Test.java
+++ src/test/java/Practice/Demo1Test.java
@@ -5,7 +5,8 @@ import org.testng.annotations.Test;
 public class Demo1Test {
    @Test
    public void demo1() {
+	   String s = "Hi";
 	   System.out.println("us 1");
-	   System.out.println("s 2");
+	   System.out.println(hi);
    }
 }
